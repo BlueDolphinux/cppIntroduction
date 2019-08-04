16d581b9fb4d0af83391927bf923c44c8f4951d0
BlueDolphinux <53137641+BlueDolphinux@users.noreply.github.com>
2019-08-04 22:35:01 +0900

    [update] pointerによる、「関数の値渡し」の回避方法を追加

--- pointer/pointer/main.cpp
+++ pointer/pointer/main.cpp
@@ -2,13 +2,23 @@
 
 using namespace std;
 
+// (... , int* sum, int* mul)の部分で、pointer領域を用意
+// *sumでその実態としての値を挿入
+void sum_mul(int x, int y, int* sum, int* mul) {
+	*sum = x + y;
+	*mul = x * y;
+}
+
 int main(void) {
 
 	int a = 135, b = 253, sw;
 
 	cin >> sw;
 
+	// ptrというpointer領域を確保
 	int* ptr;
+	
+	// ptrにa or bのアドレスを代入 
 	if (sw == 0)
 	{
 		ptr = &a;
@@ -19,15 +29,31 @@ int main(void) {
 	}
 	
 
+
 	cout << "a=" << a << "; b=" << b << "; sw=" << sw << endl;
 	cout << ptr << endl;
+	// アドレスを参照して実態を読む！
 	cout << *ptr << endl;
 
+	// (a or bの)実態を上書き
 	*ptr = 999;
 
 	cout << "a=" << a << "; b=" << b << "; sw=" << sw << endl;
 	cout << ptr << endl;
 	cout << *ptr << endl;
+
+	cout << "****************" << endl;
+
+	// 初期化 ((初期化しないとバグる！！！))
+	int wa = 0;
+	int seki = 0;
+
+	// pointer sumをエイリアスとして、waのアドレスを関数に入力。
+	// ==> pointer sumはwaを指すことになる
+	// 関数sum_mul内で、waにその結果が入る！
+	sum_mul(a, b, &wa, &seki);
+
+	cout << "wa:" << wa << "; seki:" << seki << endl;
 
 	return 0;
 }
